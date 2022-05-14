459fd1a9eed00d50a4284d909f266691cce9734a
SiddhanthDubey <91405555+SiddhanthDubey@users.noreply.github.com>
2022-05-14 12:54:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,55 @@
-<html>  
-<head>  
-    <title>PHP login system</title>  
-      
-    <link rel = "stylesheet" type = "text/css" href = "css\style.css">   
-</head>  
-<body>  
-    <div id = "frm">  
-        <h1>Login</h1>  
-        <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">  
-            <p>  
-                <label> UserName: </label>  
-                <input type = "text" id ="user" name  = "user" />  
-            </p>  
-            <p>  
-                <label> Password: </label>  
-                <input type = "password" id ="pass" name  = "pass" />  
-            </p>  
-            <p>     
-                <input type =  "submit" id = "btn" value = "Login" />  
-            </p>  
-        </form>  
-    </div>  
-    
-    <script>  
-            function validation()  
-            {  
-                var id=document.f1.user.value;  
-                var ps=document.f1.pass.value;  
-                if(id.length=="" && ps.length=="") {  
-                    alert("User Name and Password fields are empty");  
-                    return false;  
-                }  
-                else  
-                {  
-                    if(id.length=="") {  
-                        alert("User Name is empty");  
-                        return false;  
-                    }   
-                    if (ps.length=="") {  
-                        alert("Password field is empty");  
-                        return false;  
-                    }  
-                }                             
-            }  
-        </script>  
-</body>     
+<html>
+    <head>
+
+        <link rel="stylesheet" type="text/css" href="../css/ClassesAndID.css">
+        <title>Classes And ID</title>
+
+    </head>
+    <body>
+        <h1 class="t">My Website</h1>
+        <p>Lorem ipsum dolor sit aet, consectetur adipisicing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.</p>
+        <h3 id="h3" class="subtitle">About Me</h3>
+        <img src="../img/scenery.jpg" alt="Scenery" width="100%">
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qus nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Cillum dolore eu fugiat nulla, pariatur:</p>
+
+        <ul>
+            <li>Excepture sint occaecat</li>
+            <li>cupidatat non proident</li>
+            <li>sunt in culpa</li>
+        </ul>
+
+        <h2>Services</h2>
+
+        <table border="2">
+            <thead>
+                <tr>
+                    <td>Lorem</td>
+                    <td>Ipsum</td>
+                    <td>Dolor</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Sit</td>
+                    <td>Amet</td>
+                    <td>Consectetur</td>
+                </tr>
+                <tr>
+                    <td>Adipisicing</td>
+                    <td>Elit</td>
+                    <td>Sed</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <img src="../img/tree.jpg" alt="Island with three coconut trees">
+        <h2 id="contact-us">Contact Me</h2>
+        <form action="">
+            <input type="text" placeholder="Full Name">
+            <input type="email" placeholder="Your Email">
+            <input type="number" placeholder="Your Number">
+        </form>
+    </body>
 </html>
